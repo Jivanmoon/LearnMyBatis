ec5bbf51a9352f6b634806022f2750f0e042477c
贾一帆 <35562963+Jivanmoon@users.noreply.github.com>
2018-04-17 14:46:49 +0800

    Update creatMapper.java

--- Reverse_Engineering/src/com/jiayifan/creat/creatMapper.java
+++ Reverse_Engineering/src/com/jiayifan/creat/creatMapper.java
@@ -1,22 +1,20 @@
-package com.jiayifan.creat;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Test;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
-public class creatMapper {
-	@Test
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneratorSqlmap {
+
 	public void generator() throws Exception{
+
 		List<String> warnings = new ArrayList<String>();
 		boolean overwrite = true;
-		//指定逆向工程配置文件
-		File configFile = new File("config/generatorConfig.xml");
+		//指定 逆向工程配置文件
+		File configFile = new File("generatorConfig.xml");
 		ConfigurationParser cp = new ConfigurationParser(warnings);
 		Configuration config = cp.parseConfiguration(configFile);
 		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
@@ -24,5 +22,15 @@ public class creatMapper {
 				callback, warnings);
 		myBatisGenerator.generate(null);
 
+	} 
+	public static void main(String[] args) throws Exception {
+		try {
+			GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+			generatorSqlmap.generator();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 }
+
